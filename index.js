3126a228322dd5a3b913a59da1ea8b5edfc86f32
Michael Hranek <mhranek@comcast.net>
2022-01-08 00:36:49 -0500

    Updated classes to correctly take parameters when creating a new instance

--- index.js
+++ index.js
@@ -14,33 +14,80 @@ const employeeList = [];
 // Questions
 const managerQuestions = [
     {
-        name: 'manager-name',
+        name: 'managerName',
         type: 'input',
         message: "What is the team manager's name?"
     },
     {
-        name: 'manager-id',
+        name: 'managerId',
         type: 'input',
         message: "What is the manager's ID?"
     },
     {
-        name: 'manager-email',
+        name: 'managerEmail',
         type: 'input',
         message: "What is the manager's Email?"
     },
     {
-        name: 'office-num',
+        name: 'officeNum',
         type: 'input',
         message: "What is the manager's office number?"
     }
 ]
 
+const engineerQuestions = [
+    {
+        name: 'name',
+        type: 'input',
+        message: "What is the engineer's name?"
+    },
+    {
+        name: 'id',
+        type: 'input',
+        message: "What is the engineer's ID?"
+    },
+    {
+        name: 'email',
+        type: 'input',
+        message: "What is the engineer's Email?"
+    },
+    {
+        name: 'github',
+        type: 'input',
+        message: "What is the engineer's github?"
+    }
+]
+
+const internQuestions = [
+    {
+        name: 'name',
+        type: 'input',
+        message: "What is the intern's name?"
+    },
+    {
+        name: 'id',
+        type: 'input',
+        message: "What is the intern's ID?"
+    },
+    {
+        name: 'email',
+        type: 'input',
+        message: "What is the intern's Email?"
+    },
+    {
+        name: 'school',
+        type: 'input',
+        message: "What is the intern's school?"
+    }
+]
 // get user input
 inquirer
 .prompt(managerQuestions)
 .then((response) => {
     // response is all the manager's data
     // TODO create manager object with response, add to employee list
+    const newManager = new Manager(response.managerName, response.managerId, response.managerEmail, response.officeNum);
+    employeeList.push(newManager);
 
     // TODO then prompt user to pick between adding new engineer, intern, or no new employee
     promptAgain();
@@ -53,15 +100,24 @@ function promptAgain() {
             type: 'list',
             message: "What kind of employee do you want to add?",
             choices: ['Engineer', 'Intern', 'I do not want to add another employee'],
-        }]).then((reponse) => {
-            if (reponse.choice === 'Engineer') {
-                // TODO if engineer, prompt based on engineer questions
-                
+        }]).then((response) => {
+            if (response.choice === 'Engineer') {
+                // if engineer, prompt based on engineer questions
+                inquirer.prompt(engineerQuestions).then((eResp) => {
+                    // create engineer object
+                    const newEngineer = new Engineer(eResp.name, eResp.id, eResp.email, eResp.github);
+                    employeeList.push(newEngineer);
+                });
             } else if (response.choice === 'Intern') {
-                // TODO if intern, prompt based on intern questions
-
+                // if intern, prompt based on intern questions
+                inquirer.prompt(internQuestions).then((iResp) => {
+                    // create intern object
+                    const newIntern = new Intern(iResp.name, iResp.id, iResp.email, iResp.school);
+                    employeeList.push(newIntern);
+                });
             } else {
                 // TODO if no new employee finish prompt and generate HTML
+                console.log(employeeList);
             }
         })
 };
