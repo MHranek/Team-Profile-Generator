85cca061180054c0d20e2515c967824179fd7486
Michael Hranek <mhranek@comcast.net>
2022-01-07 19:09:25 -0500

    Added test html and css files to flesh out layout

---
+++ index.js
@@ -0,0 +1,28 @@
+// Requires
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+// Generates the html data
+function generateHTML() {
+    return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Team Profile</title>
+</head>
+<body>
+    
+</body>
+</html>`
+}
+
+// Generates CSS
+function generateCSS() {
+
+}
+
+// Writes html data to index.html and style.css
