f701e462c9aa8e27d3646eeee256f726127f7343
Michael Hranek <mhranek@comcast.net>
2022-01-08 00:16:45 -0500

    Beginnings of user prompt added

--- index.js
+++ index.js
@@ -6,6 +6,66 @@ const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 
+// employee list
+const employeeList = [];
+
+// Inquirer
+
+// Questions
+const managerQuestions = [
+    {
+        name: 'manager-name',
+        type: 'input',
+        message: "What is the team manager's name?"
+    },
+    {
+        name: 'manager-id',
+        type: 'input',
+        message: "What is the manager's ID?"
+    },
+    {
+        name: 'manager-email',
+        type: 'input',
+        message: "What is the manager's Email?"
+    },
+    {
+        name: 'office-num',
+        type: 'input',
+        message: "What is the manager's office number?"
+    }
+]
+
+// get user input
+inquirer
+.prompt(managerQuestions)
+.then((response) => {
+    // response is all the manager's data
+    // TODO create manager object with response, add to employee list
+
+    // TODO then prompt user to pick between adding new engineer, intern, or no new employee
+    promptAgain();
+})
+
+// prompt again
+function promptAgain() {
+    inquirer.prompt([{
+            name: 'choice',
+            type: 'list',
+            message: "What kind of employee do you want to add?",
+            choices: ['Engineer', 'Intern', 'I do not want to add another employee'],
+        }]).then((reponse) => {
+            if (reponse.choice === 'Engineer') {
+                // TODO if engineer, prompt based on engineer questions
+                
+            } else if (response.choice === 'Intern') {
+                // TODO if intern, prompt based on intern questions
+
+            } else {
+                // TODO if no new employee finish prompt and generate HTML
+            }
+        })
+};
+
 // Generates the html data
 function generateHTML() {
     return `<!DOCTYPE html>
